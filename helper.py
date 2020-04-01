632429927c348df3ccb630afddf9f8a4b4c538a5
wzn0828 <18001279185@163.com>
2020-04-01 11:46:54 +0800

    Record the output of terminal

--- helper.py
+++ helper.py
@@ -151,3 +151,20 @@ def load_cub(basedir, batch_size, kwargs):
             batch_size=batch_size, shuffle=False, **kwargs)
     
     return trainloader, testloader
+
+
+import sys
+
+class Logger(object):
+    def __init__(self, filename="Default.log"):
+        self.terminal = sys.stdout
+        self.log = open(filename, "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        pass
+
+
